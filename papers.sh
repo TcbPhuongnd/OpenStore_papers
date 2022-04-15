47fc0ae100da5c944fffc4c251c941e9710173f9
TcbPhuongnd <phuongnd@tricubics.com>
2022-04-15 11:31:02 +0900

    initial

---
+++ papers.sh
@@ -0,0 +1,60 @@
+################################ POSE3D
+# 1. Cross-View Tracking for Multi-Human 3D Pose Estimation at over 100 FPS
+# - solve the issue of camera asynchronization (not simultaneously) to update pose3D
+# - example of considering time for AFFINITY MATRIX
+# - github: https://github.com/longcw/crossview_3d_pose_tracking (no sourcecode)
+# 2. Fast and Robust Multi-Person 3D Pose Estimation from Multiple Views
+# - example of combining re-identification features & pose3d reconstruction for tracking, 
+# - solve matching by Hungarian algorithm
+# - github: https://github.com/zju3dv/mvpose
+# =>>>> The affinity measurement = feature + poseDiff(g, t)
+# 3. Real-Time Multi-View 3D Human Pose Estimation using Semantic Feedback to Smart Edge Sensors
+# - update the pose based on the weight and also track the pose
+# - github: https://github.com/AIS-Bonn/SmartEdgeSensor3DHumanPose
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
